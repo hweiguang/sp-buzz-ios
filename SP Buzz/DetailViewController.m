d8d6ea27be588f77017d14abc106e4ba1da69df9
Wei Guang <hweiguang@users.noreply.github.com>
2011-10-11 14:59:47 +0800

    Added comments

--- SP Buzz/DetailViewController.m
+++ SP Buzz/DetailViewController.m
@@ -29,22 +29,24 @@
 }
 
 - (void)reloadData {
+    //Checking for local image
     NSString *imageName = [articletitle stringByAppendingString:@".jpg"];
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
     NSString *cachesDirectory = [paths objectAtIndex:0];
     NSString *imagePath = [cachesDirectory stringByAppendingPathComponent:imageName];
-    
     NSData *imageData = [NSData dataWithContentsOfFile:imagePath];
     
     NSString *html;
     
     if (imageData) {
+        //If local image is available encode it to display in webView
         [Base64 initialize];
         NSString *Base64image = [Base64 encode:imageData];
         
         html = [NSString stringWithFormat:@"<html><head><body bgcolor='#E0FFFF'><h2><font color='red'>%@</font></h2> <img src='data:image/jpg;base64,%@' align='left'/>%@<br/><br/><br/></body></html>",articletitle,Base64image,description];
     }
     else {
+        //If local image unavailable load image from server
         html = [NSString stringWithFormat:@"<html><head><body bgcolor='#E0FFFF'><h2><font color='red'>%@</font></h2> <img src='%@' align='left'/>%@<br/><br/><br/></body></html>",articletitle,comments,description];
     }
     [webView loadHTMLString:html baseURL:nil];
@@ -52,6 +54,7 @@
 
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)_request 
  navigationType:(UIWebViewNavigationType)navigationType {
+    //If there is a link in article open it in safari
     if (navigationType == UIWebViewNavigationTypeLinkClicked) {
         [[UIApplication sharedApplication]openURL:[_request URL]];
         return  NO;
@@ -65,6 +68,7 @@
     
     switch (buttonIndex) {
         case 0: {
+            //Email
             NSURL *url = [NSURL URLWithString:link];
             SHKItem *item = [SHKItem URL:url title:articletitle];
             [SHKMail shareItem:item];
@@ -72,11 +76,11 @@
             break;
         case 1: {
             SPBuzzAppDelegate *appDelegate = (SPBuzzAppDelegate*)[UIApplication sharedApplication].delegate;
-            
+            //Check Facebook Status
             if (![appDelegate.facebook isSessionValid])
-                [appDelegate.facebook authorize:nil];
+                [appDelegate.facebook authorize:nil]; //Login Facebook
             else {
-                
+                //Post to Facebook
                 SBJSON *jsonWriter = [[SBJSON new] autorelease];
                 
                 NSDictionary *actions = [NSDictionary dictionaryWithObjectsAndKeys:@"Get SP BUZZ", @"name",
@@ -95,58 +99,64 @@
             }                        
         }
             break;
-        case 2: {/*
-                  if ([TWTweetComposeViewController canSendTweet]) { 
-                  NSURL *url = [NSURL URLWithString:[NSMutableString stringWithFormat:
-                  @"http://api.bit.ly/v3/shorten?login=%@&apikey=%@&longUrl=%@&format=txt",
-                  SHKBitLyLogin,SHKBitLyKey,link]];
-                  request = [ASIHTTPRequest requestWithURL:url];
-                  request.delegate = self;
-                  [request startAsynchronous];
-                  
-                  shorteningURLHUD = [[MBProgressHUD alloc] initWithView:self.navigationController.view];
-                  [self.navigationController.view addSubview:shorteningURLHUD];
-                  shorteningURLHUD.mode = MBProgressHUDModeIndeterminate;
-                  shorteningURLHUD.labelText = @"Shortening URL...";
-                  [shorteningURLHUD show:YES];
-                  }
-                  else {*/
-            NSURL *url = [NSURL URLWithString:link];
-            SHKItem *item = [SHKItem URL:url title:articletitle];
-            [SHKTwitter shareItem:item];
-            //}
+        case 2: {
+            //iOS 5 Twitter integration, NOT YET IMPLEMENTED in current version of SP Buzz in AppStore
+            if ([TWTweetComposeViewController canSendTweet]) { 
+                //URL for shortening News Article link
+                NSURL *url = [NSURL URLWithString:[NSMutableString stringWithFormat:
+                                                   @"http://api.bit.ly/v3/shorten?login=%@&apikey=%@&longUrl=%@&format=txt",
+                                                   SHKBitLyLogin,SHKBitLyKey,link]];
+                request = [ASIHTTPRequest requestWithURL:url];
+                request.delegate = self;
+                [request startAsynchronous];
+                
+                shorteningURLHUD = [[MBProgressHUD alloc] initWithView:self.navigationController.view];
+                [self.navigationController.view addSubview:shorteningURLHUD];
+                shorteningURLHUD.mode = MBProgressHUDModeIndeterminate;
+                shorteningURLHUD.labelText = @"Shortening URL...";
+                [shorteningURLHUD show:YES];
+            }
+            else {
+                //ShareKit Twitter sharing
+                NSURL *url = [NSURL URLWithString:link];
+                SHKItem *item = [SHKItem URL:url title:articletitle];
+                [SHKTwitter shareItem:item];
+            }
         }
             break;
         case 3: {
+            //Open link in Safari
             [[UIApplication sharedApplication] openURL:[NSURL URLWithString:link]];
         }
             break;
     }
 }
-/*
+//Successfully shorten URL
 - (void)requestFinished:(ASIHTTPRequest *)theRequest {  
     [shorteningURLHUD hide:YES];
     NSString *response = [[theRequest responseString]stringByTrimmingCharactersInSet:[NSCharacterSet newlineCharacterSet]];
     NSURL *responseURL = [NSURL URLWithString:response];
+    
+    //Create a tweet view
     TWTweetComposeViewController *tweet = [[TWTweetComposeViewController alloc]init];
     [tweet setInitialText:articletitle];
     [tweet addURL:responseURL];
     [self presentModalViewController:tweet animated:YES];
     [tweet release];
 }
-
+//Fail to shorten URL
 - (void)requestFailed:(ASIHTTPRequest *)theRequest { 
     [shorteningURLHUD hide:YES];
     MBProgressHUD *errorHUD = [[MBProgressHUD alloc] initWithView:self.navigationController.view];
-	[self.navigationController.view addSubview:errorHUD];
-	errorHUD.customView = [[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Error.png"]] autorelease];
+    [self.navigationController.view addSubview:errorHUD];
+    errorHUD.customView = [[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Error.png"]] autorelease];
     errorHUD.mode = MBProgressHUDModeCustomView;
     errorHUD.labelText = @"Shorten URL failed";
     [errorHUD show:YES];
-	[errorHUD hide:YES afterDelay:1.5];
+    [errorHUD hide:YES afterDelay:1.5];
     [errorHUD release];
 }
-*/
+
 - (void)shareButtonSelected {
     if (isActionSheetDisplayed)
         return;
@@ -162,11 +172,10 @@
     isActionSheetDisplayed = YES;
 }
 
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
+    //Setting up all the UI elements
     actionButton = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemAction 
                                                                 target:self 
                                                                 action:@selector(shareButtonSelected)];
